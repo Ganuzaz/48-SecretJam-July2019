d79a86b8d22b06cf8a01234fc6b00fed51a6fafc
Ganuzaz <Ganuzaz@Gmail.com>
2019-07-20 09:37:43 +0700

    Basic Movement and Jump

--- 42HoursSecretJam/Assets/Scripts/PlayerController.cs
+++ 42HoursSecretJam/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpForce = 5.0f;
-
+    public float groundCheckSize = 0.01f;
     private bool jumpPressed = false;
     private bool grounded = true;
     private Rigidbody2D rb;
     
+
+
     // Start is called before the first frame update
 
     private void Awake()
@@ -26,35 +28,50 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        grounded = GroundCheck();
-        rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+        Move();
+        Jump();        
+    }
 
-        if (Input.GetAxis("Vertical") > 0 && grounded)
+    void Move()
+    {
+        rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void Jump()
+    {
+        grounded = GroundCheck();
+        if (Input.GetAxisRaw("Vertical") > 0 && grounded)
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             grounded = false;
         }
-        
     }
-
     bool GroundCheck()
     {
 
 
-        Vector2 pos = new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().bounds.extents.y - 0.1f);
-        Vector2 size = new Vector2(GetComponent<BoxCollider2D>().bounds.size.x, 0.1f);
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().bounds.extents.y - groundCheckSize/2 - 0.02f);
+        Vector2 size = new Vector2(GetComponent<BoxCollider2D>().bounds.size.x, groundCheckSize);
+        
         float angle = transform.rotation.eulerAngles.z;
         var hit = Physics2D.BoxCast(pos, size, angle, new Vector2(0, 0));
 
-
+        //Debug.Log(hit.collider.name);
         if (!jumpPressed  && hit.collider != null && hit.collider.name != "Cube")
         {
             jumpPressed = true;
-            this.InvokeDelegate(() => { jumpPressed = false; }, 0.01f);
+            this.InvokeDelegate(() => { jumpPressed = false; }, 0.1f);
             return true;
         }
         else return false;
 
     }
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Vector2 pos = new Vector2(transform.position.x, transform.position.y - GetComponent<BoxCollider2D>().bounds.extents.y - groundCheckSize/2 - 0.02f);
+    //    Vector2 size = new Vector2(GetComponent<BoxCollider2D>().bounds.size.x, groundCheckSize);
+
+    //    Gizmos.DrawCube(pos, size);
+
+    //}
 }
